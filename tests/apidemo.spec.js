76c87d2618f034b51b5e62c555c670c52bb6c18b
typicalengineer008 <bishnuprasad534@gmail.com>
2026-03-23 23:06:14 +0530

    Changing the api code

--- tests/apidemo.spec.js
+++ tests/apidemo.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Get user API', async ({ request }) => {
 
 const response = await request.get(
-'https://fakestoreapi.com/products/19'
+'https://fakestoreapi.com/products'
 );
 
 expect(response.status()).toBe(200);
